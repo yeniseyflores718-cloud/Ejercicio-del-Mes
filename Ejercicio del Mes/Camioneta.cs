ab137e4ec36d863a8057d278450d66fc91166756
Juan Jose Martinez Guillen <20251014@uthh.edu.mx>
2026-06-26 10:01:07 -0600

    Clase Camioneta, y se le añaddio al formulario principal

---
+++ Ejercicio del Mes/Camioneta.cs
@@ -0,0 +1,26 @@
+﻿using Ejercicio_del_Mes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamientoWinForms
+{
+    public class Camioneta : ClaseVehiculo
+    {
+       
+        public int NumEjes { get; set; }
+
+        public Camioneta(int horasEstancia, int numeroEjes) : base(horasEstancia)
+        {
+            NumEjes = numeroEjes;
+        }
+
+        public override float CalcularTarifa()
+        {
+            return HorasEstancia * 45;
+        }
+      
+    }
+}
--- Ejercicio del Mes/ClaseVehiculo.cs
+++ Ejercicio del Mes/ClaseVehiculo.cs
@@ -18,4 +18,4 @@ namespace Ejercicio_del_Mes
     }
 
 }
-}
+
